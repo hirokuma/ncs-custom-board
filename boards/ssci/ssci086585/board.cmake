21f24e56f5caa39acd6fdaad3b72eaa34e2edec6
hirokuma <hiro9ma@gmail.com>
2024-11-21 15:38:01 +0900

    move

--- boards/ssci/ssci086585/board.cmake
+++ boards/ssci/ssci086585/board.cmake
@@ -2,9 +2,7 @@
 
 if(CONFIG_BOARD_SSCI086585_NRF5340_CPUAPP OR CONFIG_BOARD_SSCI086585_NRF5340_CPUAPP_NS)
   board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
-endif()
-
-if(CONFIG_BOARD_SSCI086585_NRF5340_CPUNET)
+endif(CONFIG_BOARD_SSCI086585_NRF5340_CPUNET)
   board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
 endif()
 
